b038caeb15ec0c1601fad7c07f96b197564bb6b9
Rowan <rowanchanner@gmail.com>
2026-09-06 02:59:18 +0100

    Disambiguate navigation tests from installed system Settings app

--- app/src/androidTest/java/com/sharky/home/LauncherSmokeTest.kt
+++ app/src/androidTest/java/com/sharky/home/LauncherSmokeTest.kt
@@ -8,19 +8,20 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.hamcrest.Matchers.equalTo
 
 @RunWith(AndroidJUnit4::class)
 class LauncherSmokeTest {
     @Test fun launchNavigateAndRecreate() {
         ActivityScenario.launch(MainActivity::class.java).use { scenario ->
             onView(withText("Home")).check(matches(isDisplayed()))
-            onView(withText("Settings")).perform(click())
+            onView(withTagValue(equalTo("nav:Settings"))).perform(click())
             onView(withText("Make it yours")).check(matches(isDisplayed()))
             scenario.recreate()
             onView(withText("Make it yours")).check(matches(isDisplayed()))
-            onView(withText("Apps")).perform(click())
+            onView(withTagValue(equalTo("nav:Apps"))).perform(click())
             onView(withText("Your apps")).check(matches(isDisplayed()))
-            onView(withText("Home")).perform(click())
+            onView(withTagValue(equalTo("nav:Home"))).perform(click())
             onView(withText("Dive into something good.")).check(matches(isDisplayed()))
         }
     }
